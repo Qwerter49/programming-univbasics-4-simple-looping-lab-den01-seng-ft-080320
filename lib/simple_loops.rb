d0ea1c9ce6d239929e9b2798b223d0d6da59d5e5
Jay Oliver <jaykamas@gmail.com>
2020-06-29 18:05:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,7 +18,7 @@ def return_string_array(array)
   counter = 0
   new_array = []
   while counter < array.length do 
-    newArray << array[counter].to_s
+    new_array << array[counter].to_s
     counter += 1 
   end
   return new_array
